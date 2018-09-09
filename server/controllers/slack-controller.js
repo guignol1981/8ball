076d4d9395468ae4c185b1b7be531f8dc5e24d8e
Vincent Guillemette <vincentguillemette1981@gmail.com>
2018-09-09 17:15:42 -0400

    Update slack-controller.js

--- server/controllers/slack-controller.js
+++ server/controllers/slack-controller.js
@@ -7,7 +7,7 @@ module.exports = class SlackController {
 		const answer = medium.getRandomAnswers();
 
 		web.chat.postMessage({
-			channel: req.body['channel_id'],
+			channel: '#random',
 			as_user: false,
 			text: `*${req.body['user_name']}* asked *${req.body['text']}*`,
 			attachments: [
