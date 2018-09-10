b5ee4201d29eb25b530499ce51bd3352fa5c43eb
vguillemette <vguillemette@devalto.com>
2018-09-09 21:15:17 -0400

    removed comments

--- server/controllers/slack-controller.js
+++ server/controllers/slack-controller.js
@@ -3,7 +3,10 @@ const medium = require('../business/medium');
 const TeamAccessToken = require('../models/team-access-token');
 
 module.exports = class SlackController {
+
 	static handleCommand(req, res) {
+		res.send();
+
 		TeamAccessToken.findByTeamId(req.body['team_id'])
 			.exec()
 			.then(teamAccessToken => {
@@ -37,13 +40,12 @@ module.exports = class SlackController {
 					.then((res) => {
 					})
 					.catch(console.error);
-
-				res.send();
-
 			});
 	}
 
 	static handleAction(req, res) {
+		res.send();
+
 		switch (req['callback_id']) {
 			case 'shake_ball':
 				TeamAccessToken.findByTeamId(req['team']['id'])
@@ -88,26 +90,3 @@ module.exports = class SlackController {
 		res.send();
 	}
 };
-
-// { type: 'interactive_message',
-// 	actions: [ { name: 'shake', type: 'button', value: 'shake' } ],
-// 	callback_id: 'shake_ball',
-// 	team: { id: 'TBZQ25QBB', domain: 'slackappplayground' },
-// 	channel: { id: 'CBZQ264EM', name: 'random' },
-// 	user: { id: 'UC1LBDH4N', name: 'vincentguillemette198' },
-// 	action_ts: '1536541793.709672',
-// 		message_ts: '1536541769.000100',
-// 	attachment_id: '1',
-// 	token: 'PUvtkr9YJZHZjYNRaI8n4gWA',
-// 	is_app_unfurl: false,
-// 	original_message:
-// 	{ text: '*vincentguillemette198* asked *test*',
-// 		username: 'SlackBall',
-// 		bot_id: 'BCQ288S10',
-// 		attachments: [ [Object] ],
-// 		type: 'message',
-// 		subtype: 'bot_message',
-// 		ts: '1536541769.000100' },
-// 	response_url:
-// 		'https://hooks.slack.com/actions/TBZQ25QBB/432082587554/u8IGeUHF0UQ0N1nqHVUI2JhP',
-// 			trigger_id: '431359294464.407818194385.fde947f8e1d54b72c1381b26556e728c' }
